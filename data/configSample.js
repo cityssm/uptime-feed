6bf69f31453454e33b9816628ead14b486fc5122
Dan Gowans <d.gowans@cityssm.on.ca>
2022-11-03 10:09:26 -0400

    sample config file

---
+++ data/configSample.js
@@ -0,0 +1,5 @@
+export const config = {
+    applicationName: "Uptime Feed - My Server",
+    httpPort: 9111
+};
+export default config;
---
+++ data/configSample.ts
@@ -0,0 +1,8 @@
+import type * as configTypes from "../types/configTypes";
+
+export const config: configTypes.Config = {
+    applicationName: "Uptime Feed - My Server",
+    httpPort: 9111
+};
+
+export default config;
