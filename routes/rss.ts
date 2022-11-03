e1a9af28f095995bc9811ef11b5968fa072b5bb6
Dan Gowans <d.gowans@cityssm.on.ca>
2022-11-03 09:43:05 -0400

    fix undefined values

--- routes/rss.ts
+++ routes/rss.ts
@@ -2,21 +2,35 @@ import { Router } from "express";
 
 import { Feed } from "feed";
 import * as configFunctions from "../helpers/functions.config.js";
-import { startupMillis } from "../helpers/functions.uptime.js";
+import { startupMillis, ipAddress } from "../helpers/functions.uptime.js";
 
 export const router = Router();
 
+const baseUrl =
+    "http://" +
+    ipAddress +
+    ":" +
+    configFunctions.getProperty("httpPort") +
+    configFunctions.getProperty("urlPrefix");
+
+const feedUrl = baseUrl + "/rss";
+const jsonUrl = baseUrl + "/json";
+
 router.all("/", (_request, response) => {
     const feed = new Feed({
-        title: configFunctions.getProperty("applicationName"),
-        id: "",
+        title: configFunctions.getProperty("applicationName") + ": " + ipAddress,
+        description: "RSS and JSON application startup notifications",
+        id: feedUrl,
+        link: feedUrl,
+        ttl: 2,
+        generator: "https://www.github.com/cityssm/uptime-feed",
         copyright: ""
     });
 
     feed.addItem({
         title: "Startup Time = " + startupMillis,
         date: new Date(startupMillis),
-        link: ""
+        link: jsonUrl + "?t=" + startupMillis
     });
 
     response.contentType("application/rss+xml");
